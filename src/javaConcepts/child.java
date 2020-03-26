f192406b2b6df0cc5154371be2f38aa89fb23c16
bindusharanurvi <55474778+bindusharanurvi@users.noreply.github.com>
2020-03-26 23:02:57 +0530

    Update child.java

--- src/javaConcepts/child.java
+++ src/javaConcepts/child.java
@@ -6,9 +6,13 @@ public class child {
 		System.out.println("this is child class");
 	}
 	
+	public void hello(){
+		system.out.println("say hello");
+	}
 	public static void main(String[] args) {
 		parent p = new parent();
 		p.ab();
 	}
+	
 
 }
